9847530f3c9def13fcf99c0e5cf0b659cb6607f7
RoopiniUKulkarni <36269667+RoopiniUKulkarni@users.noreply.github.com>
2018-07-10 12:27:20 +0530

    main method
    java codes

---
+++ std.java
@@ -0,0 +1,18 @@
+class std{
+int id;
+void getid(int id1)
+{
+id=id1;
+}
+void dis(){
+System.out.println(id);
+}
+
+public static void main(String args[]){
+std s=new std();
+s.getid(10);
+s.dis();
+
+
+}
+}
